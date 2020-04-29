163f2abc020f381230213e52ed0af656a4f53704
CHUKWUEMEKA IBEBUGWU <emex4gman@gmail.com>
2020-04-29 15:38:12 +0100

    Update

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ app =express() ;
 
 app.use(express.json({extended:true}))
 
-app.use((req, res, next)=>{
-res.setHeader("origin":"*");
-next();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+next()
 })
+
+
 
 
 
